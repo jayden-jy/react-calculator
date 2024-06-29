a2c3e8b83e8f5a86a4d50624012f4af29620ca80
jayden-jy <markup.jy@gmail.com>
2024-06-29 11:42:06 +0900

    docs: 결과 출력

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,43 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Results({ input }) {
   const resultData = calculateInvestmentResults(input);
-  console.log(resultData);
-  return <p>결과</p>;
+  const initialInvestment =
+    resultData[0].valueEndOfYear -
+    resultData[0].interest -
+    resultData[0].annualInvestment;
+
+  // console.log(resultData);
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>연도</th>
+          <th>투자 가치</th>
+          <th>이자(연도)</th>
+          <th>총 이자</th>
+          <th>투자 자본</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultData.map((YearData) => {
+          const totalInterest =
+            YearData.valueEndOfYear -
+            YearData.annualInvestment * YearData.year -
+            initialInvestment;
+          const totalAmountInvested = YearData.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={YearData.year}>
+              <td>{YearData.year}</td>
+              <td>{formatter.format(YearData.valueEndOfYear)}</td>
+              <td>{formatter.format(YearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
